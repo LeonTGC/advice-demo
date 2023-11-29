8a45e5fde9252110be3f727fc4da929cf616766f
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2023-11-29 16:20:14 +0000

    advice app finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Card from "./components/Card"
 
 const App = () => {
-  const [advice, setAdvice] = usestate('')
+  const [advice, setAdvice] = useState('')
 
-  const getAdvice = () => {
-    const response = fetch('https://api.adviceslip.com/advice')
-    const data = response.json
+  const getAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
     setAdvice(data)
   }
   useEffect(() => {
     getAdvice()
-  })
+  }, [])
 
 
-  if(advice) return <h1>loading...</h1>
+  if (!advice) return <h1>loading...</h1>
   return (
+    <div>
       <div>
-      <h1>advice app</h1>
+        <h1>advice app</h1>
       </div>
-      <Card advice={setAdvice} />
+      <Card advice={advice} getAdvice={getAdvice} />
+    </div>
   )
 }
 
+export default App
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-
+import Button from "./Button"
 
 const Card = ({ advice, getAdvice }) => {
 
     return (
         <div>
-            <h2>{advice.slip}</h2>
-            <Button func={getAdvice} txt="Get Advice" />
+            <h2>{advice.slip.advice}</h2>
+            <Button func={getAdvice} text="Get Advice" />
         </div>
     )
 }
